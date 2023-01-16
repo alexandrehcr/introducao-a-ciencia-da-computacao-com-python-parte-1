ac4f04a9ae4f095912dd0e48b94b8c86e0d19b5e
alexandrehcr <73273799+alexandrehcr@users.noreply.github.com>
2023-01-16 14:12:11 -0300

    Update week 7 - check_prime.py

--- USP - Introdução à CC com Python Parte I/week 7 - check_prime.py
+++ USP - Introdução à CC com Python Parte I/week 7 - check_prime.py
@@ -23,29 +23,3 @@ def is_prime(n):
 
 
 print("That's a prime!") if (is_prime(n)) else print("Not prime")
-
-
-# ===========================================================================
-'''I could also use the code bellow to convert the n string to integer, but it wouldn't accept negative integers.'''
-# while not n.isdigit():
-#     print("\nInsira somente números.")
-#     n = input()
-# n = int(n)
-
-# ===========================================================================
-'''Likewise, I could also declare the function as shown bellow, but it does more tests.'''
-# def is_prime(n):
-#     if n > 1:
-#         divider = 2
-
-#         while not n % divider == 0:
-#             divider += 1
-
-#         if divider == n:
-#             return True
-#         else:
-#             return False
-#     else:
-#         return False
-'''To do about the same number of tests, I could create an 'max_div = math.ceil(n / 2)' variable, put the condition 'while not n % divider and divider <= max_div' and, lastly, modify the 2nd if to be 'if divider == max_div', but it would require more lines of code as well as time to comprehend.'''
-# ===========================================================================
